e1894e752911ad5ae626aa5a98719fd3457c1d1d
Noor Fatima <noorfatima262004@gmail.com>
2026-03-11 02:41:07 +0500

    space removed

--- PaPerProjectFront/src/pages/MarketingAgentPage.jsx
+++ PaPerProjectFront/src/pages/MarketingAgentPage.jsx
@@ -247,7 +247,7 @@ const MarketingAgentPage = () => {
           ]}
         />
 
-        <div className="container mx-auto px-3 sm:px-4 py-4 sm:py-8 max-w-full overflow-x-hidden">
+        <div className="container mx-auto px-3 sm:px-4 py-4  max-w-full overflow-x-hidden">
           <Outlet />
         </div>
       </div>
