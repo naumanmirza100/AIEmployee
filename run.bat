ea19b8324edbbdf004ad62b4a0d6cb31184ae8c9
Ameer Hamza <hamzanaseer496@gmail.com>
2026-07-30 09:47:49 +0500

    Revert "First commit"
    This reverts commit 93ea6650abb30771dcdc6c62744ea9d0771b1a47.

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Start Django backend
-start "Backend" cmd /k "call venv\Scripts\activate && python manage.py runserver"
-
-:: Start React/Vite frontend
-start "Frontend" cmd /k "cd PaPerProjectFront && npm run dev"
